567152ea15f782cc750254a008c1fd5863caa7ac
Lukas Schild <47003752+luuuuk@users.noreply.github.com>
2021-12-28 23:09:28 +0100

    feat: home screen cards

--- home_data/lib/home.dart
+++ home_data/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:home_data/theme.dart';
 
 class HomePage extends StatefulWidget {
@@ -19,52 +20,164 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: ThemeColors.darkGrey,
-        child: SafeArea(
-          child: Container(
-            color: ThemeColors.lightWhite,
-            child: Row(
-              children: [
-                AnimatedContainer(
-                  width: _drawerWidth,
-                  color: ThemeColors.lightGrey,
-                  duration: const Duration(seconds: 1),
-                  curve: Curves.fastOutSlowIn,
-                  child: Column(),
-                ),
-                Expanded(
-                  child: Container(
-                    color: ThemeColors.lightWhite,
-                    child: Column(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(12.0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              const Text(
-                                "Home Data",
-                                style: TextStyle(fontSize: 21),
+      body: SafeArea(
+        child: Container(
+          color: ThemeColors.lightWhite,
+          child: Row(
+            children: [
+              AnimatedContainer(
+                width: _drawerWidth,
+                color: ThemeColors.lightGrey,
+                duration: const Duration(seconds: 1),
+                curve: Curves.fastOutSlowIn,
+                child: Column(),
+              ),
+              Expanded(
+                child: Container(
+                  color: ThemeColors.lightWhite,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              child: Row(),
+                            ),
+                            Expanded(
+                              flex: 3,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "Home Data",
+                                    style: GoogleFonts.quicksand(
+                                        fontSize: 21,
+                                        fontWeight: FontWeight.w700),
+                                  ),
+                                ],
                               ),
-                              IconButton(
-                                onPressed: () {
-                                  setState(() {
-                                    _drawerWidth =
-                                        _drawerWidth == 0.0 ? 96.0 : 0.0;
-                                  });
-                                },
-                                icon: const Icon(Icons.menu),
+                            ),
+                            Expanded(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        _drawerWidth =
+                                            _drawerWidth == 0.0 ? 96.0 : 0.0;
+                                      });
+                                    },
+                                    icon: const Icon(
+                                      Icons.menu,
+                                      color: ThemeColors.darkGrey,
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(18.0),
+                              color: ThemeColors.darkGrey,
+                            ),
                           ),
-                        )
-                      ],
-                    ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    color: ThemeColors.accentYellow,
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(12.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        const Icon(
+                                          Icons.thermostat,
+                                          color: ThemeColors.lightWhite,
+                                        ),
+                                        Text(
+                                          "Temp",
+                                          style: GoogleFonts.quicksand(
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w700,
+                                            color: ThemeColors.lightWhite,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    color: ThemeColors.midGrey,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    color: ThemeColors.midGrey,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    color: ThemeColors.midGrey,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
