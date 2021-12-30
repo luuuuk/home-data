1a722d33034e20f18fa4a361579d2af2f54e658b
Lukas Schild <47003752+luuuuk@users.noreply.github.com>
2021-12-30 19:10:04 +0100

    feat: cleanup

--- home_data/lib/widgets/graph_card.dart
+++ home_data/lib/widgets/graph_card.dart
@@ -11,7 +11,7 @@ class GraphCardWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 16.0),
+      padding: const EdgeInsets.only(top: 16.0),
       child: DataLineChart(),
     );
   }
